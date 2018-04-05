0725d794e5aa9eeb02289641ceaf9ba306617dde
artem88 <artemik2010@gmail.com>
2018-04-06 01:03:22 +0300

    Добавил интерактивный объект Настроил взаимодействие - воспроизведение анимации "по клику"

---
+++ Assets/Scripts/JallyAnimationsController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JallyAnimationsController : MonoBehaviour {
+
+    private Animator anim;
+
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	public void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("Start Animation");
+        }
+	}
+}
